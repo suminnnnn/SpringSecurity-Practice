5410b3b00bdbd018bfc514c156de6c23b44af310
suminnnnn <tlsssm1212@ajou.ac.kr>
2024-08-09 03:45:40 +0900

    chore: CORS 설정

---
+++ src/main/java/com/suminnnnn/securitypractice/global/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.suminnnnn.securitypractice.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                );
+    }
+}
